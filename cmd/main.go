6b9d464d18e7c5b6cfc00459a6f2283a0768fcad
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2025-07-12 15:54:28 -0300

    chore: remove log and fixed route

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"rinha-de-backend-2025/core/handlers"
 )
 
 func main() {
-	fmt.Println("Teste")
 	server := http.NewServeMux()
 	server.HandleFunc("POST /payments", handlers.PaymentHandler)
-	server.HandleFunc("POST /payments-summary", handlers.PaymentSummaryHandler)
+	server.HandleFunc("GET /payments-summary", handlers.PaymentSummaryHandler)
 
 	http.ListenAndServe("localhost:8080", server)
 }
