4df3685b0d7449cc7d5c7be2530fa9068bcb390e
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2025-07-14 11:53:03 -0300

    🚚 feat: add middlewares

---
+++ internal/handlers/middleware/logging.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+	"rinha-de-backend-2025/internal/config"
+	"time"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func Logging(logger *config.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			wrapper := &responseWriter{ResponseWriter: w, statusCode: 200}
+
+			next.ServeHTTP(wrapper, r)
+			duration := time.Since(start)
+			logger.Infof("\n\nmethod: %s\n path: %s\n queryparams %s\n statusCode: %d\n duration(ms): %v",
+				r.Method,
+				r.URL.Path,
+				r.URL.Query(),
+				wrapper.statusCode,
+				duration)
+
+		})
+	}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
--- internal/router/routes.go
+++ internal/router/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"rinha-de-backend-2025/internal/config"
 	"rinha-de-backend-2025/internal/handlers"
+	"rinha-de-backend-2025/internal/handlers/middleware"
 )
 
 func SetupRoutes(logger *config.Logger) http.Handler {
@@ -13,7 +14,9 @@ func SetupRoutes(logger *config.Logger) http.Handler {
 	server.HandleFunc("GET /payments-summary", handlers.PaymentSummaryHandler)
 	server.HandleFunc("GET /payments/", handlers.PaymentDetailsHandler)
 
+	handler := middleware.Logging(logger)(server)
+
 	logger.Info("All routes loaded!!!")
 
-	return server
+	return handler
 }
