86c6de97dde03c31ac4a4cc832dd308c35d4bd32
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2025-07-15 01:01:19 -0300

    :pencil2: chore: add typos

---
+++ internal/types/message.go
@@ -0,0 +1,30 @@
+package types
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Message struct {
+	ID        string    `json:"id"`
+	Content   string    `json:"content"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func NewMessage(content string) *Message {
+	return &Message{
+		ID:        randomId(),
+		Content:   content,
+		Timestamp: time.Now(),
+	}
+}
+
+func (m *Message) ToJSON() ([]byte, error) {
+	return json.Marshal(m)
+}
+
+// talvez mudar isso para um uuid?
+func randomId() string {
+	// format -> ano-mes-dia-hora-minuto-segundos : sem o hífen
+	return time.Now().Format("20060102150405") + "-" + time.Now().Format("000000")
+}
