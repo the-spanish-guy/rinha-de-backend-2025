e63816a4c82d523793cba205f160c2948e9c6fc3
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2025-07-15 09:21:43 -0300

    🧑‍ feat: add subscribe and handle message

--- internal/messaging/nats/sub.go
+++ internal/messaging/nats/sub.go
@@ -1,6 +1,9 @@
 package nats
 
 import (
+	"fmt"
+	"rinha-de-backend-2025/internal/types"
+
 	"github.com/nats-io/nats.go"
 )
 
@@ -23,4 +26,33 @@ func (s *Subscriber) Connect() error {
 
 	logger.Info("Subscriber connected to NATS")
 	return nil
+}
+
+func (s *Subscriber) Subscribe() error {
+	// Validar se existe conexão ativa?
+
+	var err error
+	s.subscription, err = s.conn.Subscribe("pub.payments", func(msg *nats.Msg) {
+		s.handleMessage(msg)
+	})
+
+	if err != nil {
+		return fmt.Errorf("falha ao se inscrever no subject: %w", err)
+	}
+
+	logger.Debugf("Inscrito no subject: %s", "pub.payments")
+	return nil
+}
+
+func (s *Subscriber) handleMessage(msg *nats.Msg) {
+	message, err := types.MessageFromJSON(msg.Data)
+	if err != nil {
+		logger.Errorf("Falha ao deserializar mensagem: %v", err)
+		return
+	}
+
+	logger.Infof("Mensagem recebida: %s", message.Content)
+
+	// processar a mensagem
+	// criar um /service/process-payments ? para lidar com a regra de negocio
 }
