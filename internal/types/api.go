44a1ed22a81e90fe191ac30f49cc9c789d5d37c8
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2025-07-21 00:02:45 -0300

    ✏️ fix: fixed typos

--- internal/types/api.go
+++ internal/types/api.go
@@ -1,11 +1,14 @@
 package types
 
+import "time"
+
 type HealtchCheckResponse struct {
 	Failing         bool `json:"failing"`
 	MinResponseTime int  `json:"minResponseTime"`
 }
 
 type PaymentsRequest struct {
-	CorrelationId string  `json:"correlationId"`
-	Amount        float64 `json:"amount"`
+	CorrelationId string    `json:"correlationId"`
+	Amount        float64   `json:"amount"`
+	RequestedAt   time.Time `json:"requestedAt"`
 }
--- internal/types/db.go
+++ internal/types/db.go
@@ -7,6 +7,7 @@ type Payments struct {
 	CorrelationId string    `json:"correlationId" db:"correlation_id"`
 	Amount        float64   `json:"amount" db:"amount"`
 	Status        string    `json:"status" db:"status"`
+	Processor     string    `json:"processor" db:"processor"`
 	RequestedAt   time.Time `json:"requestedAt" db:"requested_at"`
 	UpdatedAt     time.Time `json:"updatedAt" db:"updated_at"`
 }
